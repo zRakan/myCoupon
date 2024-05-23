880890749431366faa1a80131f525df1aefcf808
zRakan <irakanalmutairi@gmail.com>
2024-05-23 20:24:47 +0300

    Add views

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ import mongoose from 'mongoose';
 
 import session from 'express-session';
 
-import path from 'path';
-
 // Import utilities
-import { log } from './utils/index.js';
+import { isAdmin, log } from './utils/index.js';
 
 const app = express();
 
 // Express configurations
 const PORT = 3000;
 
+// Create static files
+app.use('/static', express.static('static'));
+
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(import.meta.dirname, 'views'));
 
 // Middleware(s)
 app.use(express.json());
@@ -30,8 +30,51 @@ app.use(session({
     saveUninitialized: false
 }));
 
-app.get('/', function(req, res, next) {
-    res.send(`x`);
+// Views
+import { Store, Coupon } from './models/index.js';
+app.get('/', async function(req, res) {
+    const storeList = await Store.find({}).select('name img id -_id');
+
+    res.render('index', { title: "الرئيسية", user: req.session, stores: storeList });
+});
+
+app.get('/couponsList', isAdmin, async function(req, res) {
+    let storeList = await Store.find({}).select('name img id -_id');
+        storeList = storeList.reduce((obj, val) => ({ ...obj, [val.id]: { name: val.name, img: val.img } }), {});
+
+    const notAcceptedCoupons = await Coupon.find({ accepted: false }).select('code description percentage belongs addedBy id -_id');
+
+    res.render('couponsManage', { title: "لوحة تحكم", user: req.session, couponsList: notAcceptedCoupons, stores: storeList })
+});
+
+app.get('/coupons/:store_id', async function(req, res) {
+    const storeId = req.params.store_id;
+    if(!storeId) return res.redirect('/')
+
+    const targetStore = await Store.findOne({ id: storeId }).select("id name img -_id")
+    if(!targetStore) return res.redirect('/');
+
+    const couponsList = await Coupon.find({ belongs: storeId, accepted: true }).select("code description percentage -_id");
+
+    res.render('coupons', { title: targetStore.name, user: req.session, store: targetStore, coupons: couponsList });
+})
+
+app.get('/login', function(req, res) {
+    if(req.session.loggedIn) return res.redirect('/');
+
+    res.render('authentication', { title: "تسجيل الدخول", user: req.session });
+});
+
+app.get('/register', function(req, res) {
+    if(req.session.loggedIn) return res.redirect('/');
+
+    res.render('authentication', { title: "تسجيل حساب", user: req.session });
+});
+
+app.get('/logout', function(req, res) {
+    req.session.destroy(function(err) {
+        res.redirect('/');
+    });
 });
 
 // Controller
@@ -39,6 +82,11 @@ import { couponController, storeController, userController } from './controllers
 app.use('/user', userController);
 app.use('/coupon', couponController);
 app.use('/store', storeController);
+
+// Redirect all unknown routes to index page
+app.use(function(req, res) {
+    res.redirect('/');
+})
 
 // MongoDB
 await mongoose.connect('mongodb://127.0.0.1:27017/myCoupon');
