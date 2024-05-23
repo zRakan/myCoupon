9463c4bc5b678652533c7579bd0170f6336f82df
zRakan <irakanalmutairi@gmail.com>
2024-05-24 00:03:25 +0300

    Implemented `Store creation` feature

--- controllers/store.controller.js
+++ controllers/store.controller.js
@@ -8,24 +8,44 @@ export const storeController = Router();
 function checkInputs(req, res, next) {
     const body = req.body;
 
-    if(req.url == '/create' && body.name && body.img) return next();
-    else if(req.url == '/remove' && body.id) return next();
+    if(req.url == '/remove' && body.id) return next();
 
     return apiResp["INVALID_DATA"](res);
 }
 
-storeController.post('/create', checkInputs, isAdmin, async function(req, res) {
+storeController.get('/create', isAdmin, async function(req, res) {
+    res.render('storeCreation', { title: "انشاء متجر جديد", user: req.session });
+});
+
+// Import multer & path
+import fs from 'fs/promises';
+import multer from 'multer';
+import path from 'path';
+const STORAGE = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'static/assets/images/');
+    },
+
+    filename: function(req, file, cb) {
+        cb(null, `IMAGE_${Date.now()}_${req.body.name}${path.extname(file.originalname)}`);
+    }
+});
+
+const UPLOAD = multer({ storage: STORAGE });
+
+storeController.post('/create', isAdmin, UPLOAD.single('img'), async function(req, res) {
     const body = req.body;
+    const file = req.file;
 
     try {
         const store = new Store({
             name: body.name,
-            img: body.img
+            img: `/static/assets/images/${file.filename}`
         });
 
         await store.save();
 
-        return res.json(store);
+        return res.redirect('/');
     } catch(err) {
         return apiResp["INVALID_DATA"](res);
     }
@@ -35,10 +55,17 @@ storeController.post('/remove', checkInputs, isAdmin, async function(req, res) {
     const body = req.body;
 
     try {
+        // Remove logo of the removed store
+        const targetStore = await Store.findOne({ id: body.id }).select("img -_id");
+        const IMG_PATH = `.${targetStore.img}`;
+
+        await fs.unlink(IMG_PATH);
+
+        // Remove the store data
         const removed = await Store.deleteOne({ id: body.id });
         if(!removed || removed.deletedCount == 0) return apiResp["STORE_NOT_FOUND"](res);
 
-        res.json(removed);
+        return res.json(removed);
     } catch(err) {
         return apiResp["INVALID_DATA"](res);
     }
