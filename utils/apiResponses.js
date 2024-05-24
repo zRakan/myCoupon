d7438a42ea8259239e31a7bc56f285578116804a
zRakan <irakanalmutairi@gmail.com>
2024-05-24 19:00:47 +0300

    apiResp: Added new response & fixed a typo

--- utils/apiResponses.js
+++ utils/apiResponses.js
@@ -11,12 +11,14 @@ export default {
 
     "USER_NOT_FOUND": function(res) { return res.status(HTTP_CODES['AUTHORIZATION']).send({ status: "error", message: "اسم مستخدم/كلمة مرور خاطئة" }) },
     
-    "COUPON_NOT_FOUND": function(res) { return res.status(HTTP_CODES['NOT_FOUND']).send({ status: "error", mesasge: "لم يتم العثور على كوبون الخصم" }) },
-    "STORE_NOT_FOUND": function(res) { return res.status(HTTP_CODES['NOT_FOUND']).send({ status: "error", mesasge: "لم يتم العثور على المتجر" })},
+    "COUPON_NOT_FOUND": function(res) { return res.status(HTTP_CODES['NOT_FOUND']).send({ status: "error", message: "لم يتم العثور على كوبون الخصم" }) },
+    "STORE_NOT_FOUND": function(res) { return res.status(HTTP_CODES['NOT_FOUND']).send({ status: "error", message: "لم يتم العثور على المتجر" })},
 
     "USERNAME_IS_TAKEN": function(res) { return res.status(HTTP_CODES['AUTHORIZATION']).send({ status: "error", message: "اسم مستخدم غير مُتّاح" })},
     "EMAIL_IS_TAKEN": function(res) { return res.status(HTTP_CODES['AUTHORIZATION']).send({ status: "error", message: "إيميل غير مُتّاح" })},
 
     "LOGIN_SUCCESS": function(res) { return res.status(HTTP_CODES['OK']).send({ status: 'success', message: "تم تسجيل الدخول بنجاح" }) },
     "LOGOUT_SUCCESS": function(res) { return res.status(HTTP_CODES['OK']).send({ status: 'success', message: "تم تسجيل الخروج بنجاح" }) },
+
+    "COUPON_ACCEPTED": function(res) { return res.status(HTTP_CODES['OK']).send({ status: 'success', message: "تم قبول كود الخصم بنجاح" }) }
 }
